b5139510f4316f01fad23013c22d8d35078afa1d
박찬석 <chanseok2323@gmail.com>
2021-10-02 12:36:18 +0900

    컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 생성한다.

--- src/main/java/baseball/domain/RandomGenerator.java
+++ src/main/java/baseball/domain/RandomGenerator.java
@@ -1,4 +1,4 @@
-package baseball.utils;
+package baseball.domain;
 
 import nextstep.utils.Randoms;
 
@@ -6,10 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomGenerator {
+
+    private static final int START_INDEX = 1;
+    private static final int FINAL_INDEX = 9;
+
     public static List<Integer> createRandom() {
         List<Integer> result = new ArrayList<>();
         while(result.size() < 3) {
-            int number = Randoms.pickNumberInRange(1, 9);
+            int number = Randoms.pickNumberInRange(START_INDEX, FINAL_INDEX);
             if(!result.contains(number)) {
                 result.add(number);
             }
