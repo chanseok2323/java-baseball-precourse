bfdc6fcd97381732c923b829fc5ab8b60fa11bb5
박찬석 <chanseok2323@gmail.com>
2021-10-04 00:44:38 +0900

    상수 모음 클래스

---
+++ src/main/java/baseball/common/Const.java
@@ -0,0 +1,11 @@
+package baseball.common;
+
+public class Const {
+    public static final String NOTING_MSG = "낫싱";
+    public static final String STRIKE_MSG = " 스트라이크";
+    public static final String BALL_MSG = " 볼";
+    public static final String LIST_SIZE_ERROR_MSG = "[ERROR] 세 개의 숫자를 입력해주세요.";
+    public static final String VALUE_NULL_MSG = "[ERROR] 빈 값을 입력하면 안 됩니다.";
+    public static final String VALUE_REDUPLICATION_MSG = "[ERROR] 입력한 값은 중복이 되어서는 안됩니다.";
+    public static final String FINISH_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
